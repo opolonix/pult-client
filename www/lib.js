825cf366b08665aa4ab875ac9eee93102df759b8
OPOLO <106403765+opolonix@users.noreply.github.com>
2024-05-03 10:57:03 +0300

    Delete www directory

--- www/lib.js
+++
@@ -1,9 +0,0 @@
-const len = (obj) => {return obj.length};
-const getParams = (path = null, params) => {
-    str = []
-    for (const param in params) {
-        str.push(`${encodeURIComponent(param)}=${encodeURIComponent(params[param])}`) 
-    }
-    str = str.join('&')
-    return path == null ? str : path + '?' + str
-}
